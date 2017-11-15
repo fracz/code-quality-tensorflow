b795e0a67ef9e86608b4caf0c623f73a5413e542
Wojciech Frącz <fraczwojciech@gmail.com>
2017-11-15 23:01:08 +0100

    init.sh +x

---
+++ init.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+cd "$(dirname "$0")"
+unset PYTHONPATH
+module load plgrid/apps/cuda/8.0
+module load plgrid/tools/python/3.6.0
+source venv/bin/activate
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/net/people/plgfracz/cudnn/cuda/lib64
