64590a719f8d85f546098526dcf7962182c9fe2a
Wojciech Fracz <plgfracz@login01.pro.cyfronet.pl>
2017-11-16 22:15:03 +0100

    24hr in batch

--- batch.sh
+++ batch.sh
@@ -4,9 +4,9 @@
 ## Liczba zadań per węzeł (domyślnie jest to liczba alokowanych rdzeni na węźle)
 #SBATCH --ntasks-per-node=1
 ## Ilość pamięci przypadającej na jeden rdzeń obliczeniowy (domyślnie 5GB na rdzeń)
-#SBATCH --mem-per-cpu=4092mb
+#SBATCH --mem-per-cpu=4GB
 ## Maksymalny czas trwania zlecenia (format HH:MM:SS)
-#SBATCH --time=0:05:00
+#SBATCH --time=24:00:00
 ## Nazwa grantu do rozliczenia zużycia zasobów
 #SBATCH -A scqfracz
 ## Specyfikacja partycji
