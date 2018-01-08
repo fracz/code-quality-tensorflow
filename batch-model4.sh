de6b11b96331bac32853f5e91aabe2ac7bb388c6
Wojciech Frącz <fraczwojciech@gmail.com>
2018-01-08 12:35:45 +0100

    noonlyadddel models

--- batch-model4.sh
+++ batch-model4.sh
@@ -12,7 +12,7 @@
 ## Specyfikacja partycji
 #SBATCH -p plgrid-gpu
 ## Parametr wyznaczający indeksy zadania tablicowego
-#SBATCH --array=2,3,4,5,6
+#SBATCH --array=9,10
 #SBATCH --gres=gpu:1
 
 export DATASET1=100-diff10-java-strict
@@ -23,6 +23,8 @@ export DATASET5=100-diff10-php-strict
 export DATASET6=100-diff10to50-php-strict
 export DATASET7=100-diff10-php-strict-no-parenthesis
 export DATASET8=100-diff10to50-php-strict-no-parenthesis
+export DATASET9=200-diff5to100-java-strict-noonlyadddel
+export DATASET10=100-diff10to50-java-strict-noonlyadddel
 
 
 export CURRENT_DATASET_VARIABLE=DATASET$SLURM_ARRAY_TASK_ID
